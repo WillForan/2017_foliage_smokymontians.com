b79ff6823e24ad62af6dc12767a5d5d70ec25ddc
WillForan <willforan@gmail.com>
2017-10-03 16:41:46 -0400

    loop through states and weekends

--- mkkml.bash
+++ mkkml.bash
@@ -3,7 +3,7 @@
 ########
 # make kml of fall foliage from dynamic https://smokymountains.com/fall-foliage-map 
 # USAGE:
-#   ./mkkml.bash 42 > pa.kml
+#   ./mkkml.bash > leaves.kml
 ###
 #  scrapped foliage data like:
 #    # firefox dev console -> clipboard
@@ -12,30 +12,12 @@
 #    xclip -o|jq '.[]|@csv' -r|sed 's/"//g; s/f-/f/; > foliage_20171015.csv
 ###
 
-# pa=42
-# wv=54
-state=42
-fallweekfile=~/foliage_20171015.csv
-statekml=cb_2016_${state}_cousub_500k.kml
-
-## create files
-
-#perl -ne 'if(m/((SimpleData name="(NAME|COUNTYFP)")|coordinates)>(.*)</){print $4; print m/coordinates/?"\n":"\t"}' $statekml |
-#   cut -d$'\t' -s -f1- |sed "s/^/$state/" > $state.coord
-
-# get outerBoundary for each FIPS, output
-#  STATE+COUNTYFIPS, NAME, coords
-perl -Mojo -E 'say x(f("'$statekml'")->slurp)->find("Placemark")->map(sub {$c=$_->at("outerBoundaryIs")->at("coordinates");$f=$_->at("SimpleData[name=\"COUNTYFP\"]"); $s=$_->at("SimpleData[name=\"STATEFP\"]"); $n=$_->at("SimpleData[name=\"NAME\"]"); join("\t",$s->text.$f->text,$n->text,$c->text )})->join("\n")' > $state.cord
-
-# merge coords with style from smokymountains.com
-Rscript -e "f<-read.table('$fallweekfile',sep=',',quote=''); m<-read.table('$state.coord',sep='\t',quote=''); a<-merge(f,m,by='V1',all=F); write.table(a,file='$state.stylecord',row.names=F,col.names=F,sep='\t',quote=F)" 
-
 ## header of kml
 cat <<HEREDOC
 <?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:atom="http://www.w3.org/2005/Atom" xmlns="http://www.opengis.net/kml/2.2">
 <Document>
-<name>state $state</name>
+<name>Leaves</name>
 <visibility>1</visibility>
 HEREDOC
 
@@ -56,37 +38,64 @@ for i in $(seq 1 $[${#colors[@]}-1]); do
 HEREDOC
 done
 
-cat <<HEREDOC
-<Folder id="$state leaves">
- <name>leaves</name>
-HEREDOC
+
+# pa=42
+# wv=54
+for weekend in 1015 1022; do
+   # start weekend
+   echo "<Folder id='$weekend' > <name>2017$weekend</name>"
+
+   for state in 54 42; do
+      # start state
+      echo "<Folder id='${state}_$weekend' > <name>$state 2017$weekend</name>"
+
+      fallweekfile=foliage_2017$weekend.csv
+      statekml=cb_2016_${state}_cousub_500k.kml
+      for fvar in fallweekfile statekml; do 
+          [ ! -r ${!fvar} ] && echo "cannot read $fvar: ${!fvar}" >&2 && continue 2
+      done
+
+      ## create files
+      #perl -ne 'if(m/((SimpleData name="(NAME|COUNTYFP)")|coordinates)>(.*)</){print $4; print m/coordinates/?"\n":"\t"}' $statekml |
+      #   cut -d$'\t' -s -f1- |sed "s/^/$state/" > $state.coord
+      
+      # get outerBoundary for each FIPS, output
+      #  STATE+COUNTYFIPS, NAME, coords
+      perl -Mojo -E 'say x(f("'$statekml'")->slurp)->find("Placemark")->map(sub {$c=$_->at("outerBoundaryIs")->at("coordinates");$f=$_->at("SimpleData[name=\"COUNTYFP\"]"); $s=$_->at("SimpleData[name=\"STATEFP\"]"); $n=$_->at("SimpleData[name=\"NAME\"]"); join("\t",$s->text.$f->text,$n->text,$c->text )})->join("\n")' > $state.coord
+      
+      # merge coords with style from smokymountains.com
+      Rscript -e "f<-read.table('$fallweekfile',sep=',',quote=''); m<-read.table('$state.coord',sep='\t',quote=''); a<-merge(f,m,by='V1',all=F); write.table(a,file='$state.stylecord',row.names=F,col.names=F,sep='\t',quote=F)" 
+      
 
 
-## for each place
-#join -j1 <(sed 's/,/\t/g' ~/foliage_20171015.csv|sort) <(cut -f1-3 -s -d$'\t' pa.coord|sort) | 
-cat $state.stylecord |
- while IFS=$'\t' read fips style name coord; do
- cat <<HEREDOC
- <Placemark id="$name$fips">
-    <name>$name - $fips</name>
-    <visibility>1</visibility>
-    <styleUrl>#$style</styleUrl>
-    <Polygon>
-        <extrude>0</extrude>
-        <tessellate>1</tessellate>
-        <altitudeMode>clampToGround</altitudeMode>
-        <outerBoundaryIs>
-           <LinearRing>
-              <coordinates>$coord</coordinates>
-           </LinearRing>
-        </outerBoundaryIs>
-     </Polygon>
- </Placemark>
+      ## for each place
+      #join -j1 <(sed 's/,/\t/g' ~/foliage_20171015.csv|sort) <(cut -f1-3 -s -d$'\t' pa.coord|sort) | 
+      cat $state.stylecord |
+       while IFS=$'\t' read fips style name coord; do
+       cat <<HEREDOC
+          <Placemark id="$name$fips">
+             <name>$name - $fips</name>
+             <visibility>1</visibility>
+             <styleUrl>#$style</styleUrl>
+             <Polygon>
+                 <extrude>0</extrude>
+                 <tessellate>1</tessellate>
+                 <altitudeMode>clampToGround</altitudeMode>
+                 <outerBoundaryIs>
+                    <LinearRing>
+                       <coordinates>$coord</coordinates>
+                    </LinearRing>
+                 </outerBoundaryIs>
+              </Polygon>
+          </Placemark>
 HEREDOC
+       done
+       # finish state
+       echo "</Folder>"
+    done
+    # finish weekend
+    echo "</Folder>"
 done
 
 # FINISH kml
-echo "
-</Folder>
-</Document></kml>
-"
+echo "</Document></kml>"
